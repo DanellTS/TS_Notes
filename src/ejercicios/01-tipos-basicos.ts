22303dc41a9b9e0955f1c3d0677b1c7050cf03a8
DanellTS <danell.ts@gmail.com>
2023-03-16 20:48:41 -0400

    tipos basicos ts

---
+++ src/ejercicios/01-tipos-basicos.ts
@@ -0,0 +1,11 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+ let nombre: string = 'Danell';
+ let hp: number | string = 95;
+ let estavivo: boolean = true;
+
+ hp = "full";
+
+ console.log(hp, nombre);
